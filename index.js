5bc974c06e87d3313962ef2167831c0e2d195cec
schinak-m <shinak.x3@gmail.com>
2024-02-23 21:21:30 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob'
+const leastFavoriteCustomer = 'Fred'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+}
+
+bestCustomer;
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'Bob'
+    return leastFavoriteCustomer
+}
